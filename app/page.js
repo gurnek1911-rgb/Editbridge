478b25b2641949911b96e9c27081e0d73427aafe
gurnek1911-rgb <gurnek1911@gmail.com>
2026-04-23 11:16:28 +0530

    Create app/page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>EditBridge</h1>
+
+      <a href="/login">Login</a><br/>
+      <a href="/chat">Chat</a><br/>
+      <a href="/payment">Payment</a><br/>
+      <a href="/admin">Admin</a>
+    </div>
+  );
+}
