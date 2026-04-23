4397552862ed642e811d470cecd6d9c16b3ad909
gurnek1911-rgb <gurnek1911@gmail.com>
2026-04-23 11:21:35 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,30 @@
 "use client";
 
-import
+import { useState } from "react";
+import { auth } from "../../lib/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Login</h1>
+
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} /><br/>
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} /><br/>
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
